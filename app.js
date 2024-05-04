41684ab42be734ea1e69cb099dd5d019562d2dc0
muhammadkashif20 <kashif28@gmail.com>
2024-05-03 20:00:51 -0700

    1 pattern added

--- app.js
+++ app.js
@@ -14,15 +14,28 @@ for(var i=0;i<=5;i++){
     }
     document.write('<br>')
 }
+document.write('<br>')
 
 // pattern 3
 
-for(var i=1;i<5;i++){
-    for(var j=0;j<i;j++){
-        document.write(i)
+for(var i=1;i<=5;i++){
+    // document.write(i,'<br>')
+    for(var j=1;j<=i;j++){
+        document.write(j)
     }
     document.write('<br>')
 }
+document.write('<br>')
 
+// pattern 4
+
+for(var i=0;i<5;i++){
+    document.write('A ')
+    for(var j=0;j<i;j++){
+        document.write('B ')
+    } 
+        document.write('<br>')
+    }
+    
 
 
