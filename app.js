eeb1071e3f4454819f053516a132cad65b207843
muhammadkashif20 <kashif28@gmail.com>
2024-05-06 06:43:21 -0700

    complete

--- app.js
+++ app.js
@@ -28,14 +28,97 @@ for(var i=1;i<=5;i++){
 document.write('<br>')
 
 // pattern 4
-
-for(var i=0;i<5;i++){
-    document.write('A ')
+var alphabets=['A','B','C','D','E']
+for(var i=0;i<=5;i++){
     for(var j=0;j<i;j++){
-        document.write('B ')
-    } 
-        document.write('<br>')
+        document.write(alphabets[j])
+      
     }
+    document.write('<br>')
+}
+// pattern 5
+document.write('<br>')
+document.write('<br>')
+
+for(var i=1;i<10;i+=2){
+    for(var j=1;j<=i;j++){
+        document.write('*')
+}
+document.write('<br>')
+
+}
+// pattern 6
+document.write('<br>')
+document.write('<br>')
+
+for(var i=9;i>=1;i-=2){
+    for(var j=1;j<=i;j++){
+        document.write('*')
+    }
+    document.write('<br>')
+}
+document.write('<br>')
+document.write('<br>')
+
+// pattern 7
+
+// for(var i=1;i<=5;i++){
+//     // document.write(i)
+//     for(var j=1;j<=i;j++){
+//         document.write(i)
+//         document.write(j)
+//     }
+//     document.write('<br>')
+// }
+
+document.write('<br>')
+document.write('<br>')
+// pattern 8
+var alphabets=['A','B','C','D','E','F']
+for(var i=6;i>0;i--){
+    for(var j=0;j<i;j++){
+        document.write(alphabets[j])
+      
+    }
+    document.write('<br>')
+}
+
+document.write('<br>')
+document.write('<br>')
+// pattern 8
+
+// for(var i=1;i<=5;i++){
+//     // document.write(i)
+//     for(var j=1;j<i;j++){
+//         document.write(j)
+//     }
+//     document.write('<br>')
+// }
+
+// pattern 9
+
+for(var i=4;i>=1;i--){
+    document.write('*')
+    for(var j=1;j<i;j++){
+        document.write('*')
+    }
+    document.write('<br>')
+}
+for(var i=4;i>=1;i--){
+    document.write('*')
+    for(var j=1;j<i;j++){
+        document.write('*')
+    }
+    document.write('<br>')
+}
+
+
+
+
+
+
+
+
     
 
 
